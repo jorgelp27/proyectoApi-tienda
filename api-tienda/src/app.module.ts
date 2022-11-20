e829cf8496faaa61e9102593fa672f5dd83e5072
Jorge <jorgeperez793@gmail.com>
2022-11-20 19:56:53 +0100

    Tercer_commit_conexionbd_tienda_20_11_22

--- api-tienda/src/app.module.ts
+++ api-tienda/src/app.module.ts
@@ -5,9 +5,21 @@ import { ClientesModule } from './clientes/clientes.module';
 import { ProductosModule } from './productos/productos.module';
 import { ProveedorModule } from './proveedor/proveedor.module';
 import { CategoriaModule } from './categoria/categoria.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ClientesModule, ProductosModule, ProveedorModule, CategoriaModule],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: !!process.env.DB_SYNC
+    }),
+  ClientesModule, ProductosModule, ProveedorModule, CategoriaModule,],
   controllers: [AppController],
   providers: [AppService],
 })
--- api-tienda/src/clientes/controller/clientes.controller.ts
+++ api-tienda/src/clientes/controller/clientes.controller.ts
@@ -21,6 +21,5 @@ export class clientesController {
     }
 
 
-    
 
 }
